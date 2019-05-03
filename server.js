5868502641d1fdc28ed8a8677a6c07c70937fc8a
Vanessa Markwardt <vmarkwardt@web.de>
2019-05-03 11:51:34 +0200

    Add main files

---
+++ server.js
@@ -0,0 +1,8 @@
+// Build the API:
+
+// GET / cards → returns Array of cards
+// GET / cards /: id → returns card with title, description, category
+// POST / cards → adds a card to cards
+// DELETE / cards /: id → removes a card from cards
+
+
